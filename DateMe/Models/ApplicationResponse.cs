c0a5ed7a6d00fb48d7b9fdd3c5aeca045e91b03b
kyliefromm <90372645+kyliefromm@users.noreply.github.com>
2022-01-25 12:10:34 -0700

    put in folders

---
+++ DateMe/Models/ApplicationResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DateMe.Models
+{
+    public class ApplicationResponse
+    {
+        [Key]
+        [Required]
+        public int ApplicationID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public byte Age { get; set; }
+        public string Phone { get; set; }
+        public string Major { get; set; }
+        public bool CreeperStalker { get; set; }
+            
+    
+    }
+}
---
+++ DateMe/Models/DateApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace DateMe.Models
+{
+
+    public class DateApplicationContext: DbContext
+    {
+        //constructor
+       public DateApplicationContext(DbContextOptions<DateApplicationContext> options) : base (options)
+        {
+            //leave blank for now
+        }
+        public DbSet<DateApplicationContext> responses { get; set; }
+    }
+}
